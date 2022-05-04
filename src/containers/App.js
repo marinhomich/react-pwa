37a034340af5be0e947a5b5cd4ba2fa26c56fda2
Michel Marinho <michel.marinho04@outlook.com>
2022-05-04 14:07:40 -0400

    Application Structure

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import logo from '../logo.svg';
+import Header from '../components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+     < Header logo={logo} />
+    </div>
+  );
+}
+
+export default App;
